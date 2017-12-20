68fcb75e078b062908e8ec423c2bde4f6f47777e
PhilippTh <philipp.thumfart@me.com>
2017-12-20 14:37:02 +0100

    Weather condition Change
    Added differentiation between adjective and noun and made the final sentence about the weather condition sound better.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pyowm import OWM
 from pyowm import timeutils
 
 location = 'Vienna,AT'
-owm = OWM('weather api to be added later')
+owm = OWM('weather api from openweathermap here')
 forecast = owm.daily_forecast(location)
 
 früh = timeutils.tomorrow(8, 00)
@@ -17,55 +17,70 @@ abend = timeutils.tomorrow(19, 00)
 
 
 if forecast.will_be_sunny_at(früh):
-        f = "sonnig"
+        f_adj = "sonnig"
+        f_nom = "Sonne"
 elif forecast.will_be_rainy_at(früh):
-        f = "regnerisch"
+        f_adj = "regnerisch"
+        f_nom = "Regen"
 elif forecast.will_be_foggy_at(früh):
-        f = "nebelich"
+        f_adj = "nebelich"
+        f_nom = "Nebel"
 elif forecast.will_be_cloudy_at(früh):
-        f = "bewölkt"
+        f_adj = "bewölkt"
+        f_nom = "Wolken"
 elif forecast.will_be_snowy_at(früh):
-        f = "verschneit"
+        f_adj = "verschneit"
+        f_nom = "Schnee"
 else:
         print ("Konnte keine Wetterdaten für den Morgen finden!")
         sys.exit()
 
 if forecast.will_be_sunny_at(mittag):
-        m = "sonnig"
+        m_adj = "sonnig"
+        m_nom = "Sonne"
 elif forecast.will_be_rainy_at(mittag):
-        m = "regnerisch"
+        m_adj = "regnerisch"
+        m_nom = "Regen"
 elif forecast.will_be_foggy_at(mittag):
-        m = "nebelich"
+        m_adj = "nebelich"
+        m_nom = "Nebel"
 elif forecast.will_be_cloudy_at(mittag):
-        m = "bewölkt"
+        m_adj = "bewölkt"
+        m_nom = "Wolken"
 elif forecast.will_be_snowy_at(mittag):
-        m = "verschneit"
+        m_adj = "verschneit"
+        m_nom = "Schnee"
 else:
         print ("Konnte keine Wetterdaten für Mittag finden!")
         sys.exit()
 
 if forecast.will_be_sunny_at(abend):
-        a = "sonnig"
+        a_adj = "sonnig"
+        a_nom = "Sonne"
 elif forecast.will_be_rainy_at(abend):
-        a = "regnerisch"
+        a_adj = "regnerisch"
+        a_nom = "Regen"
 elif forecast.will_be_foggy_at(abend):
-        a = "nebelich"
+        a_adj = "nebelich"
+        a_nom = "Nebel"
 elif forecast.will_be_cloudy_at(abend):
-        a = "bewölkt"
+        a_adj = "bewölkt"
+        a_nom = "Wolken"
 elif forecast.will_be_snowy_at(abend):
-        a = "verschneit"
+        a_adj = "verschneit"
+        a_nom = "Schnee"
 else:
         print ("Konnte keine Wetterdaten für den Abend finden!")
         sys.exit()
 
-if f == m == a:
-        wetter = ("Morgen wird es genztägig " + f + "."
-elif f == m:
-        wetter = "Morgen wird es vormittags " + f + " und zu späterer Stunde " + a + "."
-elif m == a:
-        wetter = "Morgen wird es in der früh " + f + " und ab der Mittagszeit " + m + "."
+if f_adj == m_adj == a_adj:
+        wetter = ("Der morgige Tag wird ganztägig überwiegend " + f_adj + "."
+elif f_adj == m_adj:
+        wetter = "Der morgige Tag beginnt überwiegend " + f_adj + " mit " + a_nom + " in den Abendstunden."
+elif m_adj == a_adj:
+        wetter = "Der morgige Tag beginnt überwiegend " + f_adj + " mit " + m_nom + " ab der Mittagszeit."
 else:
-        wetter = "Morgen wird es vormittags " + f + ", ab Mittag " + m + " und am Abend " + a + "."
+        wetter = "Der morgige Tag beginnt überwiegend " + f_adj + ", mit " + m_nom + " um die Mittagszeit und " + a_nom + " in den Abendstunden."
 
 #Twitter API
 apiKey = 'to be added later'
